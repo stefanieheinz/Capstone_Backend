dc5a92c761c1036bb82282258bb107e45d7b5f29
Stefanie <stefanieheinz0@gmail.com>
2021-06-17 19:20:16 -0600

    Create Bar Model

---
+++ db/migrate/20210618011838_create_bars.rb
@@ -0,0 +1,10 @@
+class CreateBars < ActiveRecord::Migration[6.1]
+  def change
+    create_table :bars do |t|
+      t.string :name
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
