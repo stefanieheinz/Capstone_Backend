d1b384a9ec4efd512fdfd6731c7be4282443fc1b
Stefanie <stefanieheinz0@gmail.com>
2021-06-24 18:45:41 -0600

    Add create users action and route

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  validtaes :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 
   has_many :crawls
 end
