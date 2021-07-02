c80a0407ea482891bb170b124f444f5b13fd7923
Stefanie <stefanieheinz0@gmail.com>
2021-07-01 20:15:24 -0600

    Added crawls, bars, and bar crawls controllers

---
+++ app/controllers/bar_controller.rb
@@ -0,0 +1,4 @@
+class BarController < ApplicationController
+  def Crawls
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # get 'bar/Crawls'
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
+  get "/c"
 end
