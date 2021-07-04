5c82b3e5d4b6606ebdf65c70b80a4d30ec640ec4
Stefanie <stefanieheinz0@gmail.com>
2021-07-04 09:41:49 -0600

    Add index and create actions for Crawls

--- app/controllers/crawls_controller.rb
+++ app/controllers/crawls_controller.rb
@@ -1,2 +1,15 @@
 class CrawlsController < ApplicationController
+  def index
+    crawls = Crawl.all
+    render json: crawls.as_json
+  end
+
+  def create
+    crawl = Crawl.new(
+      date: params[:date],
+      name: params[:name],
+    )
+    crawl.save
+    render json: crawl.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # get 'bar/Crawls'
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
-  get "/c"
+  get "/crawls" => "crawls#index"
+  post "/crawls" => "crawls#create"
+  get "/bars" => "bars#index"
+  post "/bar_crawls" "bar_crawls#create"
 end
